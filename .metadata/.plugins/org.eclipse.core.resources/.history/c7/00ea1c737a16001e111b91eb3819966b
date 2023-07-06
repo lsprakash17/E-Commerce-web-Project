5ecfed3ad3550a62700d3d9702c71f1d57722d16
lsprakash17 <lspraksh17@gmail.com>
2023-07-06 07:27:48 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c7/00ea1c737a16001e111b91eb3819966b
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>${name} Home</title>
+</head>
+<body>
+<h1>${pass}</h1>
+<h2>This is admin page</h2>
+
+</body>
+</html>
---
+++ E-cart/src/main/java/com/prjct/emarket/dto/Merchant.java
@@ -0,0 +1,35 @@
+package com.prjct.emarket.dto;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
+
+@Entity
+@Component
+@Data
+public class Merchant 
+{
+private String name;
+@Id
+private String email;
+private long mobile;
+private String password;
+private LocalDate dob;
+private String gender;
+private String address;
+private boolean status;
+
+private int otp;
+
+@Lob
+@Column(columnDefinition = "MEDIUMBLOB")
+byte[] picture;
+
+
+}
